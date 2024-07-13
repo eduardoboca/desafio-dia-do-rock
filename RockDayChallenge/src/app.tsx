7c2587533d31ce5940a9a683f6ff5bfded54f9a4
Igor Santiago <contato.igorsantiago@outlook.com>
2024-07-13 19:28:18 -0300

    Add react-router-dom and add basics to the light and dark theme

--- RockDayChallenge/src/app.tsx
+++ RockDayChallenge/src/app.tsx
@@ -1,5 +1,17 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { NavbarSearch } from "./pages/navbar-search";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <h1 className="flex justify-center py-10 text-4xl">Hello RockDay Challenge</h1>
+    },
+    {
+        path: "/navbar",
+        element: <NavbarSearch />
+    }
+])
+
 export function App() {
-  return (
-    <h1 className="text-3xl font-bold underline flex justify-center py-20">Hello RockDay Challenge</h1>
-  )
+    return <RouterProvider router={router} />
 }
---
+++ RockDayChallenge/src/pages/navbar-search/index.tsx
@@ -0,0 +1,3 @@
+export function NavbarSearch() {
+    return <h1>Teste</h1>
+}
