35afd46fceae37c22f66eb0c7201dfe0d6221036
Igor Santiago <contato.igorsantiago@outlook.com>
2024-07-13 20:39:03 -0300

    Add Icon Package

--- RockDayChallenge/src/app.tsx
+++ RockDayChallenge/src/app.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { NavbarSearch } from "./pages/navbar-search";
+import { Rocket } from "lucide-react"
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <h1 className="flex justify-center py-10 text-4xl">Hello RockDay Challenge</h1>
+        element: <h1 className="flex justify-center py-10 text-4xl gap-5"><Rocket />Hello RockDay Challenge<Rocket /></h1>
     },
     {
         path: "/navbar",
